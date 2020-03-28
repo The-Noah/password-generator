541961f5cbcd21d0594430abe20778f725b09248
The Noah <thenoahbz@gmail.com>
2020-03-27 23:21:05 -0500

    Fix uglify-js

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 rm .gitignore
 mkdir -p build
-npm install node-sass uglify-js
+npm install node-sass terser
 ./node_modules/node-sass/bin/node-sass --output-style=compressed style.scss > build/style.css
-./node_modules/uglify-js/bin/uglifyjs --compress --mangle -- app.js > build/app.js
+./node_modules/terser/bin/terser --compress --mangle -- app.js > build/app.js
 cp index.html build
