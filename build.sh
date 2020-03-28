fab45cd23dc9fa599deb4d4fc1efb4478ebb343c
The Noah <thenoahbz@gmail.com>
2020-03-27 23:38:39 -0500

    Add autoprefixer for css

--- build.sh
+++ build.sh
@@ -2,7 +2,8 @@
 
 rm .gitignore
 mkdir -p build
-npm install node-sass terser
+npm install node-sass terser postcss-cli autoprefixer
 ./node_modules/node-sass/bin/node-sass --output-style=compressed style.scss > build/style.css
 ./node_modules/terser/bin/terser --compress --mangle -- app.js > build/app.js
+./node_modules/postcss-cli/bin/postcss build/style.css --use autoprefixer -d build/
 cp index.html build
