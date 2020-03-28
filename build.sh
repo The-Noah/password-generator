acc20e913768858933032eb39fc135e653aa87af
The Noah <thenoahbz@gmail.com>
2020-03-27 23:10:54 -0500

    Add build script

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+mkdir -p build
+npm install node-sass uglify-js
+./node_modules/node-sass/bin/node-sass --output-style=compressed style.scss > build/style.css
+./node_modules/uglify-js/bin/uglifyjs --compress --mangle -- app.js > build/app.js
+cp index.html build
